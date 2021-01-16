0f6ca855517688a369cb755c0f1f90688bbfde59
gitupxm163 <wakeupxm@163.com>
2021-01-16 23:52:06 +0800

    init

---
+++ src/main/java/com/example/gitpod/GitpodApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitpod/GitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
